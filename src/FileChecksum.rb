1a1e51f135364c7b72fc8cdffaa6fd2bb0c6f797
Gordon Durand <gordon_durand@yahoo.com>
2010-07-15 13:23:19 -0700

    Add MPL license

--- src/FileChecksum.rb
+++ src/FileChecksum.rb
@@ -1,8 +1,27 @@
 #
+# ***** BEGIN LICENSE BLOCK *****
+# The contents of this file are subject to the Mozilla Public License
+# Version 1.1 (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+# 
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+# License for the specific language governing rights and limitations
+# under the License.
+# 
+# The Original Code is BrowserPlus (tm).
+# 
+# The Initial Developer of the Original Code is Yahoo!.
+# Portions created by Yahoo! are Copyright (c) 2010 Yahoo! Inc.
+# All rights reserved.
+# 
+# Contributor(s): 
+# ***** END LICENSE BLOCK *****
+
+#
 # A simple BrowserPlus service implemented in Ruby that will calculate the
 # MD5 sum of a file that a user has selected
-#
-# (c) Yahoo! Inc. 2008
 #
   
 class FileChecksumInstance
